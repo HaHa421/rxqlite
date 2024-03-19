dd255b9215684cc4e937dc5ccbe80921114cb92e
Your Name <you@example.com>
2024-03-19 13:20:35 +0100

    .

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ rustup default  stable-x86_64-pc-windows-msvc
 set PATH=%PATH%;C:\Program Files\LLVM\bin
 
 cargo build --release
-cargo build --release --example client
+cargo build --release --example rxqlite-client
 
 rustup default  stable-x86_64-pc-windows-gnu
 
